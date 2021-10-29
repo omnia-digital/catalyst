6fcb536ade11fced29f3d85e8d19f283477b9ba2
Josh Torres <josht@omnia-app.org>
2021-10-29 09:44:17 -0700

    - fix search styling

--- resources/js/Layouts/SocialAppLayout.vue
+++ resources/js/Layouts/SocialAppLayout.vue
@@ -29,7 +29,7 @@
                                         <SearchIcon class="h-5 w-5 dark:text-gray-400" aria-hidden="true"/>
                                     </div>
                                     <input id="search" name="search"
-                                           class="block w-full pl-10 pr-3 py-2 border border-transparent rounded-md leading-5 bg-gray-700 text-gray-300 placeholder-gray-400 focus:outline-none focus:bg-white focus:border-white focus:ring-white focus:text-gray-900 sm:text-sm"
+                                           class="block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md leading-5 dark:bg-gray-700 text-gray-300 placeholder-gray-400 focus:outline-none focus:bg-white focus:border-white focus:ring-white focus:text-gray-900 sm:text-sm"
                                            placeholder="Search" type="search"/>
                                 </div>
                             </div>
