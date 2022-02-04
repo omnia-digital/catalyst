a1618bddf5f77a5a5a078f7f5f571d2b100b3277
Josh Torres <josht@omniadigital.io>
2022-02-03 23:28:27 -0800

    update grid component

--- resources/views/components/lists/grid.blade.php
+++ resources/views/components/lists/grid.blade.php
@@ -1,10 +1,14 @@
+@props([
+    'items',
+    'selectedItem'
+])
 <ul role="list" class="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6  md:grid-cols-3 lg:grid-cols-2 xl:grid-cols-3 xl:gap-x-8">
     @foreach($items as $item)
         <x-lists.grid-item
             wire:key="item-{{ $item->id }}"
             wire:click="selectItem({{ $item->id }})"
             :item="$item"
-            :selected="$item->id === $selectedItem"
+{{--            :selected="$item->id === $selectedItem"--}}
         />
     @endforeach
 </ul>
