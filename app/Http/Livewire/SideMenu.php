a88955cf287afeb72ca7f9c3f05d162315737baa
Josh Torres <josht@omniadigital.io>
2022-02-05 09:52:44 -0800

    - update side menu projects route name

--- app/Http/Livewire/SideMenu.php
+++ app/Http/Livewire/SideMenu.php
@@ -32,7 +32,7 @@
                 ],
                 [
                     'label'   => 'Projects',
-                    'name'    => 'projects',
+                    'name'    => 'projects.home',
                     'icon'    => 'heroicon-o-collection',
                     'current' => false
                 ],
