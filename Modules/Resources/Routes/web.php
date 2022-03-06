849811b94d4f31e3367caa9c546744ac42480a8f
Josh Torres <josht@omniadigital.io>
2022-03-06 14:28:01 -0800

    - merged in feature/add_resource

--- Modules/Resources/Routes/web.php
+++ Modules/Resources/Routes/web.php
@@ -1,21 +1,14 @@
 <?php
 
-    /*
-    |--------------------------------------------------------------------------
-    | Web Routes
-    |--------------------------------------------------------------------------
-    |
-    | Here is where you can register web routes for your application. These
-    | routes are loaded by the RouteServiceProvider within a group which
-    | contains the "web" middleware group. Now create something great!
-    |
-    */
+use Modules\Resources\Http\Livewire\ListResources;
+use Modules\Resource\Http\Livewire\ShowResource;
+use Modules\Resources\Http\Livewire\Pages\Browse;
+use Modules\Resources\Http\Livewire\Pages\Home;
 
-    use Modules\Resources\Http\Livewire\Pages\Browse;
-    use Modules\Resources\Http\Livewire\Pages\Home;
-
-    Route::name('resources.')->prefix('resources')->middleware(['auth','verified'])->group(function () {
-        Route::get('/', Home::class)->name('home');
-        Route::get('browse', Browse::class)->name('browse');
-        Route::get('bookmarks', Home::class)->name('bookmarks');
-    });
+Route::name('resources.')->prefix('resources')->middleware(['auth', 'verified'])->group(function () {
+    Route::get('/', Home::class)->name('home');
+    Route::get('browse', Browse::class)->name('browse');
+    Route::get('bookmarks', Home::class)->name('bookmarks');
+    Route::get('/', ListResources::class)->name('resources.list-resources');
+    Route::get('/{resource}', ShowResource::class)->name('resources.show-resource');
+});
