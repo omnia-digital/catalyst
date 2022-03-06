25d97feda0957b830c9fe2a5c0308a31873fb4ad
Josh Torres <josht@omniadigital.io>
2022-03-06 15:31:11 -0800

    CU-2atdpz6 - move side-menu to side-menu-old to fit what it is - move SideMenu2 to be SideMenu because it's the main side menu now.

--- app/Http/Livewire/SideMenu.php
+++ app/Http/Livewire/SideMenu.php
@@ -1,74 +1,44 @@
 <?php
 
-    namespace App\Http\Livewire;
+namespace App\Http\Livewire;
 
-    use Livewire\Component;
+use Livewire\Component;
 
-    class SideMenu extends Component
+/**
+ * Side Menu should pull from Module's navigation
+ */
+class SideMenu extends Component
+{
+    public $isOpen = false;
+
+    public $navigation = [];
+
+    public function mount()
     {
-        public $isOpen = false;
-
-        public $navigation = [];
-
-        public function mount() {
+        if (empty($this->navigation)) {
             $this->navigation = [
                 [
-                    'label'   => 'Community',
+                    'label'   => 'No Module Navigation Items',
                     'name'    => 'social.home',
-                    'icon'    => 'heroicon-o-users',
+                    'icon'    => 'heroicon-o-x',
                     'current' => false
                 ],
-                [
-                    'label'   => 'Notifications',
-                    'name'    => 'notifications',
-                    'icon'    => 'heroicon-o-bell',
-                    'current' => false
-                ],
-                [
-                    'label'   => 'Messages',
-                    'name'    => 'messages',
-                    'icon'    => 'heroicon-o-mail',
-                    'current' => false
-                ],
-                [
-                    'label'   => 'Projects',
-                    'name'    => 'projects',
-                    'icon'    => 'heroicon-o-collection',
-                    'current' => false
-                ],
-                [
-                    'label'   => 'Groups',
-                    'name'    => 'groups',
-                    'icon'    => 'heroicon-o-information-circle',
-                    'current' => false
-                ],
-                [
-                    'label'   => 'Learn',
-                    'name'    => 'learn',
-                    'icon'    => 'heroicon-o-academic-cap',
-                    'current' => false
-                ],
-                [
-                    'label'   => 'Marketplace',
-                    'name'    => 'marketplace',
-                    'icon'    => 'heroicon-o-office-building',
-                    'current' => false
-                ]
             ];
         }
-
-        public function openMobileMenu()
-        {
-            $this->isOpen = true;
-        }
-
-        public function closeMobileMenu()
-        {
-            $this->isOpen = false;
-        }
-
-        public function render()
-        {
-            return view('livewire.side-menu');
-        }
     }
+
+    public function openMobileMenu()
+    {
+        $this->isOpen = true;
+    }
+
+    public function closeMobileMenu()
+    {
+        $this->isOpen = false;
+    }
+
+    public function render()
+    {
+        return view('livewire.side-menu2');
+    }
+}
