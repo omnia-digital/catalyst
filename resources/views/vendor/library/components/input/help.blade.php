70402f9433f79705aa5cdabccd4ad5e75e5aa6cf
Josh Torres <josht@omniadigital.io>
2022-03-09 15:13:54 -0800

    - remove published omnia library components since we are using the default versions right now

--- resources/views/vendor/library/components/input/help.blade.php
+++
@@ -1,5 +0,0 @@
-@props(['value'])
-
-<p {{ $attributes->merge(['class' => 'mt-2 text-sm text-gray-500']) }}>
-    {{ $value ?? $slot }}
-</p>
--- resources/views/vendor/library/components/tab/panel.blade.php
+++
@@ -1,8 +0,0 @@
-<section
-    x-show="isSelected($id('tab', whichChild($el, $el.parentElement)))"
-    :aria-labelledby="$id('tab', whichChild($el, $el.parentElement))"
-    role="tabpanel"
-    {{ $attributes->merge(['class' => 'p-8']) }}
->
-    {{ $slot }}
-</section>
