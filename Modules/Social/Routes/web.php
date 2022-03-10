c327cae39db6dab07a15948bbb2c5107f89dd2af
Josh Torres <josht@omniadigital.io>
2022-03-09 17:19:19 -0800

    - update route formatting

--- Modules/Social/Routes/web.php
+++ Modules/Social/Routes/web.php
@@ -1,50 +1,45 @@
 <?php
 
-    use Illuminate\Support\Facades\Route;
-    use Modules\Social\Http\Livewire\Home;
+use Illuminate\Support\Facades\Route;
+use Modules\Social\Http\Livewire\Home;
 use Modules\Social\Http\Livewire\Pages\Bookmarks\Index;
+use Modules\Social\Http\Livewire\Pages\Posts\Show;
 
-    Route::name('social.')->prefix('social')->middleware(['auth', 'verified'])->group(function () {
-//        Route::get('/', 'SocialController@index');
+Route::name('social.')->prefix('social')->middleware(['auth', 'verified'])->group(function () {
+    //        Route::get('/', 'SocialController@index');
 
-        // the way twitter works is
-        // /{handle} for profile
-        // /{handle}/status/{post_id} for any type of post, whether it's a post or reply
-        // /{messages}/{message_id} for messages
+    // the way twitter works is
+    // /{handle} for profile
+    // /{handle}/status/{post_id} for any type of post, whether it's a post or reply
+    // /{messages}/{message_id} for messages
 
-        Route::get('/home', Home::class)->name('home');
-        Route::get('bookmarks', Index::class)->name('bookmarks');
+    Route::get('/home', Home::class)->name('home');
+    Route::get('bookmarks', Index::class)->name('bookmarks');
 
-//        Route::get('/projects', function () {
-//            return "Projects";
-//        })->name('projects');
+    //        Route::get('/projects', function () {
+    //            return "Projects";
+    //        })->name('projects');
 
-        Route::get('/posts', function () {
-            return "Posts";
-        })->name('posts');
+    Route::get('/posts/{post}', Show::class)->name('posts.show');
 
-        Route::get('/posts/{post}', function () {
-            return "Show a post";
-        })->name('posts.show');
+    Route::get('/crm', function () {
+        return "CRM";
+    })->name('crm');
 
-        Route::get('/crm', function () {
-            return "CRM";
-        })->name('crm');
+    Route::get('/learn', function () {
+        return "Learn";
+    })->name('learn');
 
-        Route::get('/learn', function () {
-            return "Learn";
-        })->name('learn');
-
-        Route::get('/marketplace', function () {
-            return "Marketplace";
-        })->name('marketplace');
-//        Route::get('/', function () {
-////            return Inertia::render('Social/Home');
-//        })->name('social-home');
-//        Route::get('/explore', function () {
-//        })->name('explore');
-//        Route::get('/profile', [Profile::class, 'show'])->name('profile');
-//        Route::get('/bookmarks', function () {
-//        })->name('bookmarks');
-    });
+    Route::get('/marketplace', function () {
+        return "Marketplace";
+    })->name('marketplace');
+    //        Route::get('/', function () {
+    ////            return Inertia::render('Social/Home');
+    //        })->name('social-home');
+    //        Route::get('/explore', function () {
+    //        })->name('explore');
+    //        Route::get('/profile', [Profile::class, 'show'])->name('profile');
+    //        Route::get('/bookmarks', function () {
+    //        })->name('bookmarks');
+});
 
