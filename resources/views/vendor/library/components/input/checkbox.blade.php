7743614d3266d96f4b2aebe517efbf42a7b9648f
Osimba <osiqua@gmail.com>
2022-03-17 09:44:32 -0700

    CU-2579q39 - Removed library - Removed package-lock

--- resources/views/vendor/library/components/input/checkbox.blade.php
+++
@@ -1,1 +0,0 @@
-<input type="checkbox" {{ $attributes->merge(['class' => 'focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded']) }}>
--- resources/views/vendor/library/components/tag.blade.php
+++
@@ -1,3 +0,0 @@
-<span {{ $attributes->merge(['class' => 'inline-flex items-center px-2 py-1 rounded-lg font-medium']) }}>
-  {{ $slot }}
-</span>
