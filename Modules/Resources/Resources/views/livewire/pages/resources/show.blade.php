2e32db56c63baa9c3d7a374279aea968b319e175
phucle <phuclh93@gmail.com>
2022-03-22 21:04:45 -0700

    Use Josh's show resource template

--- Modules/Resources/Resources/views/livewire/pages/resources/show.blade.php
+++ Modules/Resources/Resources/views/livewire/pages/resources/show.blade.php
@@ -3,83 +3,49 @@
 @section('content')
     <div class="mb-4 flex items-center">
         <div class="mr-4 hover:bg-gray-300 p-2 rounded-full">
-            @if ($resource->isParent())
-                <a href="{{ route('resources.home') }}">
-                    <x-heroicon-o-arrow-left class="h-6"/>
-                </a>
-            @else
-                <a href="{{ route('resources.show', $resource->postable) }}">
-                    <x-heroicon-o-arrow-left class="h-6"/>
-                </a>
-            @endif
+            <a href="{{ route('resources.home') }}">
+                <x-heroicon-o-arrow-left class="h-6"/>
+            </a>
         </div>
-
         <h1 class="py-2 text-3xl">Resource</h1>
     </div>
     <div class="xl:grid xl:grid-cols-9 xl:gap-9">
         <div class="xl:col-span-6">
-            <div class="bg-white px-4 py-5 sm:px-6 rounded-lg">
-                <div class="flex space-x-3">
-                    <div class="flex-shrink-0">
-                        <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="{{ $resource->user->profile->name }}">
-                    </div>
-                    <div class="min-w-0 flex-1">
-                        <p class="text-sm font-medium text-gray-900">
-                            <a href="#" class="hover:underline">{{ $resource->user->profile->name }}</a>
-                        </p>
-                        <p class="text-sm text-gray-500">
-                            <a href="#" class="hover:underline">{{ $resource->created_at->diffForHumans() }}</a>
-                        </p>
-                    </div>
-                    <div class="flex-shrink-0 self-center flex">
-                        <div class="relative z-30 inline-block text-left">
-                            <x-library::dropdown>
-                                <x-slot name="trigger">
-                                    <button type="button" class="-m-2 p-2 rounded-full flex items-center text-gray-400 hover:text-gray-600" id="menu-0-button" aria-expanded="false" aria-haspopup="true">
-                                        <span class="sr-only">Open options</span>
-                                        <x-heroicon-s-dots-vertical class="h-5 w-5"/>
-                                    </button>
-                                </x-slot>
-                                <x-library::dropdown.item>Report</x-library::dropdown.item>
-                            </x-library::dropdown>
-                        </div>
-                    </div>
+            <div>
+                <img class="rounded-lg w-full object-cover max-h-96 bg-gray-300 flex-shrink-0" src="{{$resource->image}}" alt="{{$resource->title}}">
+            </div>
+            <div class="flex mt-6">
+                <h3 class="text-gray-900 text-4xl hover:underline font-bold">{{ $resource->title }}</h3>
+                @empty(!$resource->is_verified)
+                    <x-heroicon-o-check-circle class="flex-shrink-0 w-6 h-6 inline-block  text-green-700 text-xs font-medium rounded-full"/>
+                @endempty
+            </div>
+            <div class="flex justify-start my-2">
+                <x-heroicon-o-calendar class="w-5 h-5"/>
+                <p class="ml-2 text-gray-500 text-md truncate">{{ $resource->created_at->format('M d, Y') }}</p>
+            </div>
+            @empty(!$resource->tags)
+                <div class="flex justify-start space-x-2">
+                    @foreach($resource->tags as $tag)
+                        <x-library::tag>{{ $tag }}</x-library::tag>
+                    @endforeach
                 </div>
+            @endempty
 
-                <div class="flex items-center justify-between py-4">
-                    <div class="flex items-center space-x-3">
-                        <x-library::heading.4>{{ $resource->title }}</x-library::heading.4>
+            <div class="text-xl my-6">
+                {!! Purify::clean($resource->body) !!}
+            </div>
 
-                        @empty(!$resource->is_verified)
-                            <x-heroicon-o-check-circle class="flex-shrink-0 w-6 h-6 inline-block  text-green-700 text-xs font-medium rounded-full"/>
-                        @endempty
-                    </div>
-
-                    @if ($resource->tags)
-                        <div class="flex justify-start space-x-2">
-                            @foreach($post->tags as $tag)
-                                <x-library::tag class="bg-gray-200">{{ $tag }}</x-library::tag>
-                            @endforeach
-                        </div>
-                    @endif
-                </div>
-
-                <div class="w-full pb-2">
-                    {!! Purify::clean($resource->body) !!}
-                </div>
-
-                <div class="block w-full aspect-w-10 aspect-h-3 rounded-lg overflow-hidden">
-                    <img src="{{ $resource->image }}" alt="{{ $resource->title }}" class="object-cover">
-                </div>
-
-                <div class="px-6">
-                    <livewire:social::partials.post-actions :post="$resource"/>
-                </div>
-
+            <div class="py-4">
                 @if ($resource->isParent())
                     <livewire:social::comment-section :post="$resource" :type="\Modules\Social\Enums\PostType::RESOURCE"/>
                 @endif
             </div>
+
+            <a href="{{ $resource->url }}" target="_blank" class="bg-white hover:shadow-lg rounded-lg px-4 py-2 text-xl inline-flex items-center space-x-2">
+                <p>Go to Resource</p>
+                <x-heroicon-o-arrow-right class="h-6 w-6"/>
+            </a>
         </div>
 
         <aside class="hidden xl:block xl:col-span-3">
