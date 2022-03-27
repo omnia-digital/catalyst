f7d09aba20c567cae76084f221d097728b93c107
phucle <phuclh93@gmail.com>
2022-03-27 09:22:29 -0700

    Make home use the new post comment and purify content

--- Modules/Social/Resources/views/livewire/home.blade.php
+++ Modules/Social/Resources/views/livewire/home.blade.php
@@ -1,55 +1,51 @@
 @extends('social::livewire.layouts.main-layout')
 
 @section('content')
-<div>
-    <!-- Page Heading -->
-    <div class="xl:grid xl:grid-cols-9 xl:gap-9">
-        <div class="xl:col-span-6">
-            <div x-data="setup()">
-                <ul class="flex justify-center items-center my-4">
-                    <template x-for="(tab, index) in tabs" :key="tab.id">
-                        <li class="flex flex-1 text-sm cursor-pointer py-2 px-6 text-gray-500 border-b-2 justify-center"
-                            :class="activeTab===tab.id ? 'text-black font-bold border-black' : ''"
-                            @click="activeTab = tab.id"
-                            x-html="tab.title + notifications"></li>
-                    </template>
-                </ul>
+    <div>
+        <!-- Page Heading -->
+        <div class="xl:grid xl:grid-cols-9 xl:gap-9">
+            <div class="xl:col-span-6">
+                <div x-data="setup()">
+                    <ul class="flex justify-center items-center my-4">
+                        <template x-for="(tab, index) in tabs" :key="tab.id">
+                            <li class="flex flex-1 text-sm cursor-pointer py-2 px-6 text-gray-500 border-b-2 justify-center"
+                                :class="activeTab===tab.id ? 'text-black font-bold border-black' : ''"
+                                @click="activeTab = tab.id"
+                                x-html="tab.title + notifications"></li>
+                        </template>
+                    </ul>
 
-            </div>
-            <div class="mt-0">
-                <div class="mx-auto">
-                    <livewire:social::post-editor/>
+                </div>
+                <div class="mt-0">
+                    <div class="mx-auto">
+                        <livewire:social::post-editor/>
 
-                    <h1 class="sr-only">Recent Posts</h1>
+                        <h1 class="sr-only">Recent Posts</h1>
+                        <div class="mt-6 space-y-4">
+                            @if ($recentlyAddedPost)
+                                <x-social::post.card wire:key="post-{{ $post->id }}" :post="$post"/>
+                            @endif
+                            @foreach ($this->posts as $post)
+                                <x-social::post.card wire:key="post-{{ $post->id }}" :post="$post"/>
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-4">
+                    <!-- Featured Section -->
+                    <livewire:social::map/>
+
+                    <!-- Posts -->
                     <ul role="list" class="mt-6 space-y-4">
-                        @if ($recentlyAddedPost)
-                            <li role="listitem">
-                                <livewire:social::post-list-item :post="$recentlyAddedPost" :wire:key="$recentlyAddedPost->id" />
-                            </li>
-                        @endif
-                        @foreach ($this->posts as $post)
-                            <li role="listitem">
-                                <livewire:social::post-list-item :post="$post" :wire:key="$post->id" />
-                            </li>
+                        @foreach ($activities as $activity)
+                            <livewire:social::partials.activity-list-item :activity="$activity"/>
                         @endforeach
                     </ul>
                 </div>
             </div>
-            <div class="mt-4">
-                <!-- Featured Section -->
-                <livewire:social::map/>
-
-                <!-- Posts -->
-                <ul role="list" class="mt-6 space-y-4">
-                    @foreach ($activities as $activity)
-                        <livewire:social::partials.activity-list-item :activity="$activity"/>
-                    @endforeach
-                </ul>
-            </div>
+            <x-sidebar-column/>
         </div>
-        <x-sidebar-column />
     </div>
-</div>
 @endsection
 @push('scripts')
     <script>
@@ -87,4 +83,4 @@
             }
         }
     </script>
-    @endpush
+@endpush
