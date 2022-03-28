8549025d7eafbb8b505cafda9bfbd03cfc87877d
phucle <phuclh93@gmail.com>
2022-03-27 17:32:38 -0700

    Save comment from homepage

--- Modules/Social/Http/Livewire/RepliesModal.php
+++ Modules/Social/Http/Livewire/RepliesModal.php
@@ -3,27 +3,51 @@
 namespace Modules\Social\Http\Livewire;
 
 use Livewire\Component;
+use Modules\Social\Actions\CreateNewPostAction;
 use Modules\Social\Models\Post;
+use Modules\Social\Support\Livewire\WithPostEditor;
 
 class RepliesModal extends Component
 {
-    public $modalOpen = false;
-    public $replyCount;
-    public $post;
-    public $show;
-    protected $listeners = ['postAdded'];
+    use WithPostEditor;
 
-    public function postAdded(Post $post) {
-        $this->modalOpen = false;
+    public int $replyCount = 0;
+
+    public Post $post;
+
+    public bool $show;
+
+    public ?string $content = null;
+
+    protected $listeners = [
+        'postAdded',
+        'post-editor:submitted' => 'saveComment'
+    ];
+
+    public function postAdded()
+    {
         $this->replyCount = $this->post->comments()->count();
     }
 
-
-
-    public function mount($post, $show = false) {
+    public function mount($post, $show = false)
+    {
         $this->post = $post;
         $this->replyCount = $post->comments()->count();
         $this->show = $show;
+    }
+
+    public function saveComment($data)
+    {
+        $this->content = strip_tags($data['content']);
+
+        $this->validatePostEditor();
+
+        (new CreateNewPostAction)
+            ->asComment($this->post)
+            ->execute($data['content']);
+
+        $this->emitPostSaved();
+        $this->redirectRoute('social.posts.show', $this->post);
     }
 
     public function render()
