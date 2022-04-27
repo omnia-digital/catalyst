0224e8f5c26e18f8d725e2e514343a49d8d1f407
phucle <phuclh93@gmail.com>
2022-04-26 23:00:19 -0700

    CU-29wwyjk - All Projects (Browse / Search)

--- app/Http/Livewire/Teams.php
+++ app/Http/Livewire/Teams.php
@@ -3,19 +3,48 @@
 namespace App\Http\Livewire;
 
 use App\Models\Team;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Arr;
 use Livewire\Component;
 
+/**
+ * @property Builder $rowsQuery
+ * @property Collection $rows
+ */
 class Teams extends Component
 {
-    public $projects = [];
+    public array $filters = [
+        'location' => null,
+        'date' => null,
+        'members' => [0, 0],
+        'rating' => [1]
+    ];
 
-    public function mount()
+    public function getRowsQueryProperty()
     {
-        $this->projects = Team::all();
+        return Team::query()
+            ->withCount('users')
+            ->when(Arr::get($this->filters, 'location'), fn(Builder $query, $location) => $query->where('location', $location))
+            ->when(Arr::get($this->filters, 'date'), fn(Builder $query, $date) => $query->whereDate('created_at', $date))
+            ->when(Arr::get($this->filters, 'members'), fn(Builder $query, $members) => $query->havingBetween('users_count', $members))
+            ->when(Arr::get($this->filters, 'rating'), fn(Builder $query, $rating) => $query->whereIn('rating', $rating));
+    }
+
+    public function getRowsProperty()
+    {
+        return $this->rowsQuery->paginate(25);
+    }
+
+    public function filter()
+    {
+        $this->callMethod('$refresh');
     }
 
     public function render()
     {
-        return view('livewire.teams');
+        return view('livewire.teams', [
+            'projects' => $this->rows
+        ]);
     }
 }
--- resources/views/livewire/teams.blade.php
+++ resources/views/livewire/teams.blade.php
@@ -2,23 +2,52 @@
     <main class="flex-1">
         <div class="py-6">
             <div class="px-4 sm:px-6 md:px-0">
-                <h1 class="text-2xl font-semibold text-gray-900">Dashboard</h1>
+                <h1 class="text-2xl font-semibold text-gray-900">Projects</h1>
             </div>
             <div class="px-4 sm:px-6 md:px-0">
                 <div class="grid grid-cols-3 gap-4 py-4">
-                    <div class="border border-gray-200 bg-white shadow-sm rounded-md p-4">
+                    <div class="border border-gray-200 bg-white shadow-sm rounded-md p-4 min-h-screen">
                         <x-library::heading.3>Filters</x-library::heading.3>
+
+                        <div class="py-4 space-y-4">
+                            <div>
+                                <x-library::input.label>Location</x-library::input.label>
+                                <x-library::input.text wire:model.defer="filters.location" placeholder="Location"/>
+                            </div>
+
+                            <div>
+                                <x-library::input.label>Date</x-library::input.label>
+                                <x-library::input.date wire:model.defer="filters.date" placeholder="Select Date"/>
+                            </div>
+
+                            <div>
+                                <x-library::input.label>Number of Members</x-library::input.label>
+                                <x-library::input.range-slider
+                                        wire:model.defer="filters.members"
+                                        :min="0" :max="100" :step="5" :decimals="0"
+                                        showTextFields/>
+                            </div>
+
+                            <div>
+                                <x-library::input.label>Rating</x-library::input.label>
+                                <x-library::input.range-slider
+                                        wire:model.defer="filters.rating"
+                                        :min="1" :max="5" :step="1" :decimals="0" :options="['tooltips' => true]"/>
+                            </div>
+                        </div>
+
+                        <div class="mt-4">
+                            <x-library::button class="w-full" wire:click="filter" wire:target="filter">
+                                Show
+                            </x-library::button>
+                        </div>
                     </div>
                     <div class="col-span-2">
-                        <ul role="list" class="space-y-4">
+                        <div>
                             @foreach($projects as $project)
-                                <li>
-                                    <livewire:resources::components.resource-card
-                                            as="li"
-                                            :post="$project"/>
-                                </li>
+                                {{ $project->name }}
                             @endforeach
-                        </ul>
+                        </div>
                     </div>
                 </div>
             </div>
