1aa2567bdae6775e39cf72b2403249cedf56c7ef
Osimba <osiqua@gmail.com>
2022-06-08 11:15:42 -0700

    CU-2ectyt6 - Update sort styling

--- resources/views/livewire/partials/filters.blade.php
+++ resources/views/livewire/partials/filters.blade.php
@@ -1,28 +1,29 @@
-<div>
-    <div class="mb-6 border-t border-b border-gray-100 sm:flex sm:items-center sm:justify-between">
+<div class="space-y-2">
+    <div class="bg-primary px-6 rounded-lg border-t border-b border-gray-100 sm:flex sm:items-center sm:justify-between">
         <div class="flex items-center pr-8">
-            <span class="mr-3">Sort:</span>
-            <x-library::dropdown.index :position="'left'" class="z-10" :dropdownClasses="'bg-white border-none shadow-md'">
-                <x-slot:trigger class=" hover:cursor-pointer text-base-text-color hover:text-black">{{ $sortLabels[$orderBy] }}</x-slot>
+            <span class="mr-3 font-bold">Sort</span>
+            <x-library::dropdown.index :position="'left'" class="z-10 p-2 rounded-md bg-neutral" :dropdownClasses="'bg-white border-none shadow-md'">
+                <x-slot:trigger class=" hover:cursor-pointer text-base-text-color hover:text-black">
+                    {{ $sortLabels[$orderBy] }}
+                    <i class="fa-solid fa-caret-down ml-1"></i>
+                </x-slot>
                 @foreach ($sortLabels as $key => $item)
                     <x-library::dropdown.item class="bg-white border-none" wire:click.prevent="sortBy('{{ $key }}')">{{ $item }}</x-library::dropdown.item>
                 @endforeach
             </x-library::dropdown.index>
             @if (!$sortDesc)
-                <x-heroicon-o-arrow-narrow-up class="w-4 ml-2 hover:cursor-pointer text-dark-text-color" wire:click.prevent="$toggle('sortDesc')" />
+                <x-heroicon-o-arrow-narrow-up class="w-4 ml-2 hover:cursor-pointer text-base-text-color hover:text-black" wire:click.prevent="$toggle('sortDesc')" />
             @else
-                <x-heroicon-o-arrow-narrow-down class="w-4 ml-2 hover:cursor-pointer text-dark-text-color" wire:click.prevent="$toggle('sortDesc')" />
+                <x-heroicon-o-arrow-narrow-down class="w-4 ml-2 hover:cursor-pointer text-base-text-color hover:text-black" wire:click.prevent="$toggle('sortDesc')" />
             @endif
         </div>
         <div class="flex-1 min-w-0 bg-primary p-2 rounded-lg">
             <div class="filters flex flex-col md:flex-row space-y-3 md:space-y-0 md:space-x-6">
                 <div class="w-full relative">
-                    <x-library::input.text type="search" wire:model.debounce.500ms="search" placeholder="Search..." class="px-4 py-2 pl-8 bg-neutral border-1 border-secondary"/>
-                    <div class="absolute top-0 flex items-center h-full ml-3">
-                        <svg class="w-4 text-dark-text-color" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                        </svg>
+                    <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                        <x-heroicon-o-search class="h-5 w-5 text-light-text-color dark:text-light-text-color" aria-hidden="true"/>
                     </div>
+                    <x-library::input.text type="search" wire:model.debounce.500ms="search" placeholder="Search..." class="px-4 block w-full pl-10 pr-3 py-2 border border-neutral bg-neutral rounded-md leading-5 dark:bg-gray-700 text-light-text-color placeholder-light-text-color focus:outline-none focus:ring-dark-text-color sm:text-sm"/>
                 </div>
             </div>
         </div>
@@ -33,7 +34,7 @@
             Filters
         </div>
         <div class="w-full relative md:w-1/3">
-            <x-library::input.date wire:model="filters.start_date" class="pl-8" placeholder="Project Launch Date"/>
+            <x-library::input.date wire:model="filters.published_at" class="pl-8" placeholder="Select Date"/>
             <div class="absolute top-0 flex items-center h-full ml-3">
                 <x-heroicon-o-calendar class="w-4 text-dark-text-color" />
             </div>
