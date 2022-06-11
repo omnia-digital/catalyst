c3992e3c57872ced409fcaabc38cd9534716bdee
Osimba <osiqua@gmail.com>
2022-06-10 23:29:37 -0700

    CU-2ecufqv - Fix follower page issue on team profile

--- Modules/Social/Resources/views/livewire/pages/projects/followers.blade.php
+++ Modules/Social/Resources/views/livewire/pages/projects/followers.blade.php
@@ -6,7 +6,7 @@
 <div class="mt-4 -mx-4">
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-4">
         @foreach ($team->followers as $item)
-            <livewire:social::user-tile :user="$item->followable"  />
+            <x-user-tile :user="$item->follower"  />
         @endforeach
     </div>
 </div>
