9f46418a41f0adf891544b902e47608a6b5f60db
Osimba <osiqua@gmail.com>
2022-06-29 13:33:34 -0700

    CU-2579u88 - Use proper layout

--- Modules/Social/Resources/views/livewire/pages/profiles/awards.blade.php
+++ Modules/Social/Resources/views/livewire/pages/profiles/awards.blade.php
@@ -1,7 +1,8 @@
 @extends('social::livewire.layouts.pages.user-profile-layout')
 
 @section('content')
-    <div class="mt-4 -ml-4">
+    <x-profiles.partials.header :user="$this->user"/>
+    <div class="mt-4">
         <h2 class="text-black font-semibold text-2xl">Awards</h2>
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
             @foreach ($profile->awards as $award)
