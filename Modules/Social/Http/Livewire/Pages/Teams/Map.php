6a9521e8090c6ef5413b9e72de9877f9ee78d754
Josh Torres <josht@omniadigital.io>
2022-07-06 16:53:41 -0700

    update text in map error to use Trans::get

--- Modules/Social/Http/Livewire/Pages/Teams/Map.php
+++ Modules/Social/Http/Livewire/Pages/Teams/Map.php
@@ -21,7 +21,7 @@ class Map extends Component
         $team = Team::find($eventId);
 
         if (!$team || !($location = $team->location()->first()) || !($location->lng) || !($location->lat)) {
-            $this->error('Cannot find the project or location. Please refresh the page and try again!');
+            $this->error(\Trans::get('Cannot find the team or location. Please refresh the page and try again!'));
 
             return;
         }
