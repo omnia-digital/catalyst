2da9e5aab996245d64aca5635b93f3e666489b93
Osimba <osiqua@gmail.com>
2022-07-08 23:30:06 -0700

    CU-2kcmc5x - Add mapbox to home page - update team list styling

--- Modules/Social/Http/Livewire/Home.php
+++ Modules/Social/Http/Livewire/Home.php
@@ -2,10 +2,14 @@
 
 namespace Modules\Social\Http\Livewire;
 
+use App\Models\Location;
 use Livewire\Component;
+use OmniaDigital\OmniaLibrary\Livewire\WithMap;
 
 class Home extends Component
 {
+    use WithMap;
+    
     public $tabs = [];
 
     public $activities = [];
@@ -37,8 +41,33 @@ class Home extends Component
         $this->questions = [];
     }
 
+    public function getPlacesProperty()
+    {
+        $places = Location::query()
+            ->hasCoordinates()
+            ->with('model')
+            ->get()
+            ->map(function (Location $location) {
+                return [
+                    'name' => $location->model->name,
+                    'lat' => $location->lat,
+                    'lng' => $location->lng,
+                    'address' => $location->address,
+                    'address_line_2' => $location->address_line_2,
+                    'city' => $location->city,
+                    'state' => $location->state,
+                    'postal_code' => $location->postal_code,
+                    'country' => $location->country,
+                ];
+            });
+
+        return $places->all();
+    }
+
     public function render()
     {
-        return view('social::livewire.home');
+        return view('social::livewire.home', [
+            'places' => $this->places,
+        ]);
     }
 }
