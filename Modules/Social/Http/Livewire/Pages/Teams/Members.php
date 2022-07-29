2b9eb360777619d9dea9f5de0e31977ba6c61427
Osimba <osiqua@gmail.com>
2022-07-29 10:07:33 -0700

    CU-2g644bc - Set up modal for adding awards to team members - Set up dropdown to see user awards on team members list - Set up policy for only admin users on a team to be able to add awards

--- Modules/Social/Http/Livewire/Pages/Teams/Members.php
+++ Modules/Social/Http/Livewire/Pages/Teams/Members.php
@@ -3,6 +3,7 @@
 namespace Modules\Social\Http\Livewire\Pages\Teams;
 
 use App\Models\Team;
+use App\Models\User;
 use App\Traits\Team\WithTeamManagement;
 use Livewire\Component;
 
@@ -12,13 +13,29 @@ class Members extends Component
 
     public $team;
 
+    public $awardsToAdd = [];
+
     protected $listeners = [
         'member_added' => '$refresh',
+        'modal-closed' => 'resetAwardsSelection'
     ];
 
     public function mount(Team $team)
     {
         $this->team = $team;
+    }
+
+    public function resetAwardsSelection()
+    {
+        $this->reset('awardsToAdd');
+    }
+
+    public function addAward(User $member)
+    {
+        $member->awards()->attach($this->awardsToAdd);
+        
+        $this->dispatchBrowserEvent('notify', ['message' => 'Awards Added', 'type' => 'success']);
+        $this->dispatchBrowserEvent('add-awards-modal-' . $member->id,  ['type' => 'close']);
     }
 
     public function render()
