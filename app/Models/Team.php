ce254683e121a2a07aa588bc82e2498a3232e212
Osimba <osiqua@gmail.com>
2022-08-16 17:58:28 -0700

    CU-29xjgfy - Fix Accepting application error - Fix error displaying team members - Add Owner and Member to Roles

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -188,7 +188,7 @@ class Team extends JetstreamTeam implements HasMedia
 
     public function owner()
     {
-        return $this->hasOneThrough(User::class, Membership::class, 'team_id', 'id', 'id', 'user_id');
+        return $this->hasOneThrough(User::class, Membership::class, 'team_id', 'id', 'id', 'user_id')>where('role', 'owner');
     }
 
     public function members()
@@ -197,7 +197,7 @@ class Team extends JetstreamTeam implements HasMedia
     }
 
     public function allUsers() {
-        return $this->users();
+        return $this->users;
     }
 
     public function profile()
