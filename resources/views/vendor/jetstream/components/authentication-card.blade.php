66ea44f3c13a685e12cbc42934e314a945289685
Josh Torres <josht@omniadigital.io>
2022-09-14 10:22:06 -0700

    - update styling/layout and add slogan to authentication screens

--- resources/views/vendor/jetstream/components/authentication-card.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -1,19 +1,27 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-neutral">
-    <div>
-        {{ $logo }}
-    </div>
+<div class="grid grid-cols-2 bg-neutral">
+    {{-- Left --}}
+    <div class="flex flex-col items-end min-h-screen pr-24 justify-center">
+        <div class="flex flex-col">
+            <div>
+                {{ $logo }}
+            </div>
 
-    <div>
-        {{ $slogan ?? '' }}
-    </div>
-
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-primary shadow-md overflow-hidden sm:rounded-lg">
-        {{ $slot }}
-    </div>
-
-    @isset($additionalCard)
-        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-primary shadow-md overflow-hidden sm:rounded-lg">
-            {{ $additionalCard }}
+            <div>
+                {{ $slogan ?? config('app.slogan', '') }}
+            </div>
         </div>
-    @endisset
+    </div>
+    {{-- Right --}}
+    <div class="flex flex-col items-start min-h-screen pl-24 justify-center">
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-primary shadow-md overflow-hidden sm:rounded-lg">
+            {{ $slot }}
+        </div>
+
+        @isset($additionalCard)
+            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-primary shadow-md overflow-hidden sm:rounded-lg">
+                {{ $additionalCard }}
+            </div>
+        @endisset
+    </div>
+
 </div>
