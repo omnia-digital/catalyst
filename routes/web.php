0b0dd3e9030b3bec5802c5c060dd5bbe050b089e
Josh Torres <josht@omniadigital.io>
2023-01-06 14:22:53 -0800

    - add redirect web route so we can link external links through platform

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,10 @@ use App\Http\Livewire\Pages\Account\Index as AccountIndex;
 use App\Http\Livewire\UserNotifications;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/r/{url?}', function ($url) {
+    return redirect($url);
+})->where('url', '.*');
+
 Route::get('/', function () {
     return redirect()->route('social.home');
 });
@@ -19,3 +23,5 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
 // Stripe Connect
 Route::get('/teams/stripe-connect/refresh', HandleStripeConnectRefreshUrlController::class)->name('teams.stripe-connect.refresh');
+
+
