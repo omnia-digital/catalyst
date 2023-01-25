f5455ea07f1f841a989219b39a9a74c2a475eee8
Josh Torres <josht@omniadigital.io>
2023-01-25 10:09:10 -0800

    - add omnia logo to images - update mix to copy images to public/storage/images - deleted public/images directory

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,7 +16,7 @@ mix.js('resources/js/app.js', 'public/js')
         require('postcss-import'),
         require('tailwindcss'),
     ])
-    .copyDirectory('resources/images', 'public/images');
+    .copyDirectory('resources/images', 'public/storage/images');
 
 
 if (mix.inProduction()) {
