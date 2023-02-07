655e75e838b2fd0de6ccbc7afb4f5ee08752fffa
Osimba <osiqua@gmail.com>
2023-02-07 10:37:26 -0700

    pull changes from dev

--- database/factories/TeamFactory.php
+++ database/factories/TeamFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Enums\Teams\TeamRoleTypes;
 use App\Models\Team;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
@@ -31,12 +32,22 @@ class TeamFactory extends Factory
         ];
     }
 
-    public function withUsers($amount = 1, $role = 'Member')
+    public function withUsers($amount = 1, $roleName = 'Member')
     {
-        return $this->hasAttached(User::factory($amount)->withProfile(), function(Team $team) use ($role) {
+        return $this->hasAttached(User::factory($amount)->withProfile(), function(Team $team) use ($roleName) {
+            $role = Role::where('name', $roleName)->where('team_id', $team->id)->first();
             setPermissionsTeamId($team->id);
+
+            if (is_null($role)) {
+                $role = Role::create([
+                    'name' => $roleName, 
+                    'type' => strtolower($roleName),
+                    'team_id' => $team->id
+                ]);
+            }
+
             return [
-                'role_id' => Role::findOrCreate($role)->id,
+                'role_id' => $role->id,
             ];
         });
 
