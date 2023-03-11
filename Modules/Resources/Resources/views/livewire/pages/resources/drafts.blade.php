27d70b42ca8d09f9792fe148a3a81d182b4be94c
Josh Torres <josht@omniadigital.io>
2023-03-11 10:13:53 -0800

    - update drafts and published icons and titles

--- Modules/Resources/Resources/views/livewire/pages/resources/drafts.blade.php
+++ Modules/Resources/Resources/views/livewire/pages/resources/drafts.blade.php
@@ -26,9 +26,13 @@
 
     <div class="bg-secondary px-6 py-2 rounded-lg border-t border-b border-gray-100 sm:flex sm:items-center sm:justify-between">
         <nav class="flex space-x-8 py-2" aria-label="Global">
-            <a href="{{ route('resources.drafts') }}" class="bg-neutral text-base-text-color inline-flex items-center rounded-md py-2 px-3 text-sm font-medium" aria-current="page">Drafts</a>
-            <a href="{{ route('resources.published') }}" class="text-base-text-color hover:bg-neutral hover:text-base-text-color inline-flex items-center rounded-md py-2 px-3 text-sm font-medium">Published</a>
-          </nav>
+            <a href="{{ route('resources.drafts') }}" class="bg-neutral text-base-text-color inline-flex items-center rounded-md py-2 px-3 font-medium"
+               aria-current="page">
+                <x-library::icons.icon name="fa-regular fa-pen-to-square" size="w-5 h-5" class="pr-2"/>Drafts</a>
+            <a href="{{ route('resources.published') }}" class="text-base-text-color hover:bg-neutral hover:text-base-text-color inline-flex items-center rounded-md py-2 px-3
+            font-medium">
+                <x-library::icons.icon name="fa-duotone fa-newspaper" size="w-5 h-5" class="pr-2"/>Published</a>
+        </nav>
     </div>
 
     <div class="mt-4">
