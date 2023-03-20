7cb1dc3feea9a37d34fc23cf0173be414ee2594a
Josh Torres <josht@omniadigital.io>
2023-03-19 20:43:51 -0700

    - update resource media show

--- Modules/Social/Resources/views/livewire/pages/posts/show.blade.php
+++ Modules/Social/Resources/views/livewire/pages/posts/show.blade.php
@@ -4,7 +4,7 @@
     <div class="flex items-center mt-6 max-w-post-card-max-w mx-auto">
         <div class="mr-6 w-8">
             <div class="hover:bg-neutral-light p-2 rounded-full bg-secondary hover:text-secondary flex justify-center items-center">
-                <a href="{{ route('social.home') }}">
+                <a href="{{ route('resources.home') }}">
                     <x-heroicon-o-arrow-left class="h-4"/>
                 </a>
             </div>
