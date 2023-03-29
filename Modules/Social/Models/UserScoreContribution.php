aaa5eb600079a747060215d09a82bf5da82fdaf6
Osimba <osiqua@gmail.com>
2023-03-29 14:50:53 -0700

    CU-8669zrch4 - Set up user_score_contributions table - Set up UserScoreContributions model

---
+++ Modules/Social/Models/UserScoreContribution.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Modules\Social\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+
+class UserScoreContribution extends Model
+{
+    use HasFactory, HasSlug;
+
+    protected $fillable = ['name', 'slug', 'points'];
+    
+    protected static function newFactory()
+    {
+        return \Modules\Social\Database\factories\UserScoreContributionFactory::new();
+    }
+
+    public static function getPointsFor($slug)
+    {
+        $slug = Str::snake($slug);
+
+        return self::where('slug', $slug)->first()->points;
+    }
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+                          ->generateSlugsFrom('name')
+                          ->saveSlugsTo('slug')
+                          ->usingSeparator('_');
+    }
+}
