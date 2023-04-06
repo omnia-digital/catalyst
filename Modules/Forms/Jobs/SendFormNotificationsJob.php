1f7b8a8128894a5deda83604a0f6cdbbd51974e7
x7ryan <x7ryan@users.noreply.github.com>
2023-04-06 23:32:13 +0000

    Dusting

--- Modules/Forms/Jobs/SendFormNotificationsJob.php
+++ Modules/Forms/Jobs/SendFormNotificationsJob.php
@@ -3,15 +3,14 @@
 namespace Modules\Forms\Jobs;
 
 use App\Models\Team;
-use App\Models\User;
 use Carbon\Carbon;
 use Carbon\CarbonTimeZone;
 use Illuminate\Bus\Queueable;
-use Illuminate\Queue\SerializesModels;
-use Illuminate\Queue\InteractsWithQueue;
-use Modules\Forms\Models\FormNotification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Modules\Forms\Models\FormNotification;
 use Modules\Forms\Notifications\FormReminderNotification;
 
 class SendFormNotificationsJob implements ShouldQueue
