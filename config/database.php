aa43660b9e82b70a16025c98b3eab5aca84d9769
Josh Torres <josht@omniadigital.io>
2023-06-15 11:51:03 -0700

    - update redis & horizon - update github actions

--- config/database.php
+++ config/database.php
@@ -128,10 +128,11 @@ return [
 
         'default' => [
             'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'host' => env('REDIS_HOST', 'localhost'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
+            'read_write_timeout' => 60,
         ],
 
         'cache' => [
