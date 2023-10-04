cccc3a05a3bf167d3a2803f9783761413c8cb1ae
Josh Torres <josht@omniadigital.io>
2023-10-04 16:35:48 -0700

    - move non-standard files to catalyst-core-plugin - move forms module to catalyst-forms-plugin

--- Modules/Forms/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('forms::layouts.master')
-
-@section('content')
-    <x-library::heading.1>Hello World</x-library::heading.1>
-
-    <p>
-        This view is loaded from module: {!! config('forms.name') !!}
-    </p>
-@endsection
