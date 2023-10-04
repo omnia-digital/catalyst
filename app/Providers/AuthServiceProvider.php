e653650e01844327bc78a2b72403bccf1db0f7b5
Josh Torres <josht@omniadigital.io>
2023-10-04 16:37:32 -0700

    - move non-standard files to catalyst-core-plugin - move forms module to catalyst-forms-plugin

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,8 +3,6 @@
 namespace App\Providers;
 
 use App\Models\Team;
-use App\Policies\ExceptionPolicy;
-use App\Policies\PermissionPolicy;
 use App\Policies\TeamPolicy;
 use BezhanSalleh\FilamentExceptions\Models\Exception;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
@@ -12,6 +10,8 @@ use Illuminate\Validation\Rules\Password;
 use Laravel\Passport\Passport;
 use Modules\Forms\Models\FormType;
 use Modules\Forms\Policies\FormTypePolicy;
+use OmniaDigital\CatalystCore\Policies\ExceptionPolicy;
+use OmniaDigital\CatalystCore\Policies\PermissionPolicy;
 use Spatie\Permission\Models\Permission;
 
 class AuthServiceProvider extends ServiceProvider
