d562d8a3c0126b4e48172f116f218e397dd1ef04
Josh Torres <josht@omniadigital.io>
2023-10-04 15:31:42 -0700

    - move reviews module to reviews plugin - update team

--- Modules/Reviews/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('reviews::layouts.master')
-
-@section('content')
-    <x-library::heading.1>Hello World</x-library::heading.1>
-
-    <p>
-        This view is loaded from module: {!! config('reviews.name') !!}
-    </p>
-@endsection
